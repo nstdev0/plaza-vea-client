dec138fd4ef0036ff0e59f5a90dbbc9ee27031ec
nstdev0 <nst.dev0@gmail.com>
2026-01-19 14:11:19 -0500

    chore: remove leftover console.log

--- src/lib/api-client.ts
+++ src/lib/api-client.ts
@@ -4,19 +4,15 @@ const BASE_URL = AppConfig.API_URL;
 
 export async function ApiFetch(url: string) {
   try {
-    console.log("[API-CLIENT]: ", BASE_URL, url);
-
     const res = await fetch(`${BASE_URL}${url}`, {
       headers: {
         "Content-Type": "application/json",
       },
     });
-
     const jsonResponse = await res.json().catch(() => {
       console.error("Response was not valid JSON");
     });
     const apiResponse = jsonResponse;
-
     return apiResponse;
   } catch (error) {
     console.error("Error fetching API:", error);
